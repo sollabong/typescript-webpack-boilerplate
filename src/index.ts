be4f2c8fdd3cbc86c329d44f1dd046e23ab806cb
Gábor Soltész-Kéri <solteszkerigabor@outlook.hu>
2023-11-10 08:58:40 +0100

    Small refactor with types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import flagsmith from 'flagsmith';
 import 'flowbite';
 
+const freePlan = document.getElementById('free-plan') as HTMLElement;
+const pricingTable = document.getElementById('pricing-table') as HTMLElement;
+
 flagsmith.init({
   environmentID: 'dF5aF2UXeRtbCxWg47kbhY',
+
   onChange: () => {
-    const freePlan: Node | null = document.getElementById('free-plan');
-    const pricingTable: HTMLElement | null =
-      document.getElementById('pricing-table');
     if (!flagsmith.hasFeature('feature_free_plan')) {
-      (pricingTable as HTMLElement).removeChild(freePlan as Node);
-      (pricingTable as HTMLElement).classList.remove('lg:grid-cols-4');
-      (pricingTable as HTMLElement).classList.add('lg:grid-cols-3');
+      pricingTable.removeChild(freePlan);
+      pricingTable.classList.remove('lg:grid-cols-4');
+      pricingTable.classList.add('lg:grid-cols-3');
       console.log('feature_free_plan');
     }
   },
